8f4ad7d54a07016fd540ae4c98f725cdc22138f9
GobiasSomeCoffeeCo <60927833+GobiasSomeCoffeeCo@users.noreply.github.com>
2023-02-21 14:50:34 -0500

    Update README & version

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,4 @@ pub const BAD: &str = "\x1b[1;91m[-]\x1b[0m";
 pub const GOOD: &str = "\x1b[1;32m[+]\x1b[0m";
 pub const INFO: &str = "\x1b[1;93m[!]\x1b[0m";
 pub const CHECK: &str = "\x1b[1;36m[✓]\x1b[0m";
-pub const VERSION: &str = "0.1.0";
+pub const VERSION: &str = "0.2.0";
